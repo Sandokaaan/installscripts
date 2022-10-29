f5447441c846a137f18dae6c540b03c4d2b74db3
Sandokaaan <mail@deveac.com>
2022-10-29 23:20:03 +0200

    initial upload

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-#
+#! /bin/sh
+
+sudo apt-get -y update
+sudo apt-get -y install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-test-dev libboost-thread-dev libdb-dev libdb++-dev
+git clone https://github.com/Sandokaaan/Earthcoin.git
+cd Earthcoin
+./autogen.sh
+./configure --disable-tests --disable-gui-tests --with-incompatible-bdb
+make
+sudo make install
